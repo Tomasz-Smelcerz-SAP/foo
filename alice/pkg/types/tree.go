846ccdb9c1febe0ba15f7606a781eb89fe47850f
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2025-08-20 23:51:42 +0200

    restore top-level pkg

---
+++ alice/pkg/types/tree.go
@@ -0,0 +1,60 @@
+package tree
+
+import (
+	"strconv"
+)
+
+type Node struct {
+	lhc *Node
+	rhc *Node
+	value int
+}
+
+func NewNode(value int) *Node {
+	return &Node{
+		lhc:   nil,
+		rhc:   nil,
+		value: value,
+	}
+}
+
+func T(left *Node, value int, right *Node) *Node {
+	return &Node{
+		lhc:   left,
+		rhc:   right,
+		value: value,
+	}
+}
+
+func L(value int) *Node {
+	return &Node{
+		lhc:   nil,
+		rhc:   nil,
+		value: value,
+	}
+}
+
+func (n *Node) SetLeftChild(child *Node) {
+	n.lhc = child
+}
+
+func (n *Node) SetRightChild(child *Node) {
+	n.rhc = child
+}
+
+func (n *Node) String() string {
+	if n == nil {
+		return "nil"
+	}
+	var leftSubtree string
+	if n.lhc != nil {
+		leftSubtree = n.lhc.String()
+	}
+
+	var rightSubtree string
+	if n.rhc != nil {
+		rightSubtree = n.rhc.String()
+	}
+
+	return "[" + leftSubtree + "~" + strconv.FormatInt(int64(n.value), 16) + "~" + rightSubtree + "]"
+}
